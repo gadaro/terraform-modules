2e071b5724bea84233c9c3b9f5ef4544752fdb32
neogabe <gadaro@consultant.com>
2018-10-01 12:32:42 +0200

    Custom server text and AMI

--- services/webserver-cluster/user_data.sh
+++ services/webserver-cluster/user_data.sh
@@ -3,5 +3,6 @@
 yum install -y httpd
 echo "Connected to `curl -s http://169.254.169.254/latest/meta-data/local-hostname`<br>" > /var/www/html/index.html
 echo "Available db = ${db_address}:${db_port}" >> /var/www/html/index.html
+echo "${additional_text}" >> /var/www/html/index.html
 sed 's/Listen 80/Listen ${server_port}/' /etc/httpd/conf/httpd.conf -i
 service httpd start
